8806251b1683ee409021e1fd5a1bd2052c913014
JunWoo <junu.sinclair@gmail.com>
2019-01-21 22:55:06 +0900

    draw image to canvas from input files

--- src/app/ImageHandler.ts
+++ src/app/ImageHandler.ts
@@ -2,34 +2,38 @@ export default class ImageHandler {
   private bookImageInput = document.getElementById('BookImage_Input');
   private patternImageInput = document.getElementById('PatternImage_Input');
 
-  private bookImageResult = document.getElementById('Uploaded_BookImage');
-  private patternImageResult = document.getElementById('Uploaded_PatternImage');
+  private combineCanvas = document.getElementById('CombineCanvas');
+  private combineCanvasContext?: CanvasRenderingContext2D;
 
   constructor() {
     this.bookImageInput.addEventListener('change', (e: Event) => {
-      if (this.bookImageResult instanceof HTMLImageElement) {
-        this.afterBookImageInputChange(e, this.bookImageResult);
-      }
+      this.afterBookImageInputChange(e);
     });
     this.patternImageInput.addEventListener('change', (e: Event) => {
-      if (this.patternImageResult instanceof HTMLImageElement) {
-        this.afterBookImageInputChange(e, this.patternImageResult);
-      }
+        this.afterBookImageInputChange(e);
     });
+    if (this.combineCanvas instanceof HTMLCanvasElement) {
+      this.combineCanvasContext = this.combineCanvas.getContext('2d');
+    }
   }
 
-  afterBookImageInputChange(e: Event, target: HTMLImageElement) {
+  afterBookImageInputChange(e: Event) {
     const reader: FileReader = new FileReader();
     if (e.currentTarget instanceof HTMLInputElement) {
       const imageFile = e.currentTarget.files[0];
       reader.onloadend = () => {
-        target.src = `${reader.result}`;
+
+        const imageSrc = reader.result;
+        const newImageObj = new Image();
+
+        newImageObj.src = `${imageSrc}`;
+        newImageObj.onload = () => {
+          this.combineCanvasContext.drawImage(newImageObj, 0, 0);
+        }
       };
 
       if (imageFile) {
         reader.readAsDataURL(imageFile);
-      } else {
-        target.src = "";
       }
     }
   }
